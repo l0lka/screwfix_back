7a5150fb35d2f8b35cb1cd1efd4a7f6cf1dfb62c
Svitlana <sinergy.ua@gmail.com>
2018-02-06 18:37:52 +0200

    test

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,8 +3,8 @@ const passport = require('passport');
 const router = express.Router();
 
 const env = {
-  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID || 'Gn9MQKXEhKaw5DZ05kgFWkRHM3lGClEo',
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN || 'screwfix.auth0.com',
   AUTH0_CALLBACK_URL:
   process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
 };
